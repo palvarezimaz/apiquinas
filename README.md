458a45a475a3b56af1178e94025f7cad4fa92b4a
palvarzimaz <palvarezimaz@gmail.com>
2022-09-11 19:27:18 +1000

    new quotes and minor fixes + readme updated

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # APIQUINAS - Aquinas API
 
-Inspired by famous APIs like that one that delives Kayne West wisdom or that other one that tells you Dad jokes..., I wanted to do my share and build an API that could deliver content that its normaly overlooked but it's full of wisdom. Stop looking at ancient Greek stoics and epicureans, and dwelve in it's superation (Aufhebung?):
+Inspired by famous APIs like that one that delives Kayne West wisdom or that other one that tells you Dad jokes..., I wanted to do my share and build an API that could deliver content that its normaly overlooked but it's full of wisdom. Stop looking at ancient Greek stoics and epicureans, and dwelve in it's superation (Aufhebung?)..
 
-APIQUINAS will feed you with some of the best ideas from European Medieval Philosophy.
+APIQUINAS will feed you with some of the best ideas from European Medieval Philosophy!
 
 I do hope that you can learn from those great thinkers as much as I did.
 
--- views/quotes-list.js
+++ views/quotes-list.js
@@ -14,6 +14,177 @@ const quotes = [
     quote:
       'We must love them both, those whose opinions we share and those whose opinions we reject, for both have labored in the search for truth, and both have helped us in finding it.',
   },
+  {
+    author: 'Ibn Sina (Avicenna)',
+    quote:
+      'Is it the fault of wine if a fool drinks it and goes stumbling into darkness? ',
+  },
+  {
+    author: 'Ibn Sina (Avicenna)',
+    quote:
+      'Anyone who denies the law of non-contradiction should be beaten and burned until he admits that to be beaten is not the same as not to be beaten, and to be burned is not the same as not to be burned.',
+  },
+  {
+    author: 'Ibn Sina (Avicenna)',
+    quote: 'Absence of understanding does not warrant absence of existence',
+  },
+  {
+    author: 'Ibn Rushd (Averroes)',
+    quote:
+      'Ignorance leads to fear, fear leads to hate, and hate leads to violence. This is the equation.',
+  },
+  {
+    author: 'Ibn Rushd (Averroes)',
+    quote:
+      'An army of philosophers would not be sufficient to change the nature of error and to make it truth.',
+  },
+  {
+    author: 'Ibn Rushd (Averroes)',
+    quote: 'Women should be treated as human beings, not as domestic animals.',
+  },
+  {
+    author: 'Maimonides',
+    quote:
+      'We each decide whether to make ourselves learned or ignorant, compassionate or cruel, generous or miserly. No one forces us. No one decides for us, no one drags us along one path or the other. We are responsible for what we are.',
+  },
+  {
+    author: 'Maimonides',
+    quote:
+      'Truth does not become more true by virtue of the fact that the entire world agrees with it, nor less so even if the whole world disagrees with it.',
+  },
+  {
+    author: 'Maimonides',
+    quote: 'You must accept the truth from whatever source it comes.',
+  },
+  {
+    author: 'Maimonides',
+    quote:
+      'A small amount of wine such as three or four glasses is of benefit for the preservation of the health of human beings and an excellent remedy for most illnesses.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'The greatest kindness one can render to any man consists in leading him from error to truth.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'The things that we love tell us what we are.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'He who is not angry when there is just cause for anger is immoral. Why? Because anger looks to the good of justice. And if you can live amid injustice without anger, you are immoral as well as unjust.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'To love is to will the good of the other.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Fear is such a powerful emotion for humans that when we allow it to take us over, it drives compassion right out of our hearts.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Better to illuminate than merely to shine; to deliver to others contemplated truths than merely to contemplate.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Man cannot live without joy; therefore when he is deprived of true spiritual joys it is necessary that he become addicted to carnal pleasures.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'If the highest aim of a captain were to preserve his ship, he would keep it in port forever.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'Charity is the form, mover, mother and root of all the virtues.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'There is nothing on this earth more to be prized than true friendship.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'There is within every soul a thirst for happiness and meaning.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'One cannot use an evil action with reference to a good intention.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'To bear with patience wrongs done to oneself is a mark of perfection, but to bear with patience wrongs done to someone else is a mark of imperfection and even of actual sin.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Mercy without justice is the mother of dissolution; justice without mercy is cruelty.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Good can exist without evil, whereas evil cannot exist without good.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote:
+      'Friendship is the source of the greatest pleasures, and without friends even the most agreeable pursuits become tedious.',
+  },
+  {
+    author: 'Thomas Aquinas, St.',
+    quote: 'There can be no joy in living without joy in work.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote:
+      'Right is right even if no one is doing it; wrong is wrong even if everyone is doing it.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote:
+      'Hope has two beautiful daughters; their names are Anger and Courage. Anger at the way things are, and Courage to see that they do not remain as they are.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote: 'There is no saint without a past, no sinner without a future.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote:
+      'And men go abroad to admire the heights of mountains, the mighty waves of the sea, the broad tides of rivers, the compass of the ocean, and the circuits of the stars, yet pass over the mystery of themselves without a thought.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote: 'Tolle, lege: take up and read.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote: 'Sin is looking for the right thing in the wrong place.',
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote: "What are kingdoms without justice? They're just gangs of bandits.",
+  },
+  {
+    author: 'Augustine of Hippo, St.',
+    quote: 'Dilige et quod vis fac. (Love and then what you will, do.)',
+  },
+  {
+    author: 'Albert the Great, St.',
+    quote:
+      'He took over anger to intimidate subordinates, and in time anger took over him.',
+  },
+  {
+    author: 'Peter Abelard',
+    quote:
+      'The key to wisdom is this - constant and frequent questioning, for by doubting we are led to question and by questioning we arrive at the truth.',
+  },
 ];
 
 module.exports = quotes;
